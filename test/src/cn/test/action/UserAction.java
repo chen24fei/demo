9cb5dc1cbaafbbf1ddab3ea540af2183cfa47c9b
dormitory <chen_24_fei@yeah.net>
2012-04-13 23:30:17 +0800

    添加 几个校验
    UserAction的数据校验

--- test/src/cn/test/action/UserAction.java
+++ test/src/cn/test/action/UserAction.java
@@ -1,5 +1,7 @@
 package cn.test.action;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.context.annotation.Scope;
@@ -7,22 +9,45 @@ import org.springframework.stereotype.Controller;
 
 import cn.test.domain.User;
 import cn.test.service.UserService;
+import cn.test.util.ValidateUtil;
 
 @Controller("userAction")
 @Scope("prototype")
-public class UserAction extends BaseAction<User> {
-
+public class UserAction extends BaseAction<User>{
 	private static final long serialVersionUID = 5560616425783961911L;
-	
 	@Resource
-	private UserService service;
+	private UserService userService;
 	private User model = new User();
 	@Override
 	public User getModel() {
 		return model;
 	}
-	public String doTest(){
+	//注册
+	public String reg(){
+		userService.saveEntity(model);
 		return SUCCESS;
+	}
+	//验证数据有效性
+	public void validateReg(){
+		if(!ValidateUtil.isValid(model.getEmail())){
+			addFieldError("email","email必须填写！");
+		}
+		if(!ValidateUtil.isValid(model.getNickName())){
+			addFieldError("nickName", "昵称必须填写！");
+		}
+		if(!ValidateUtil.isValid(model.getPassword())){
+			addFieldError("password", "密码必须填写");
+		}
+		if(super.hasErrors()){
+			return;
+		}
+		//判断两次密码是否一致---暂时空着
+		//数据库判断
+		String hql = "from User u where u.email=?";
+		List<User> users = userService.findEntityByHQL(hql, model.getEmail());
+		if(ValidateUtil.isValid(users)){
+			addFieldError("email","该邮箱已被注册！");
+		}
 	}
 
 }
---
+++ test/src/cn/test/util/ValidateUtil.java
@@ -0,0 +1,34 @@
+package cn.test.util;
+
+import java.util.Collection;
+
+/**
+ * 校验工具类
+ * @author Administrator
+ *
+ */
+public class ValidateUtil {
+
+	/**
+	 * 判断字符串的有效性
+	 * @param str
+	 * @return
+	 */
+	public static boolean isValid(String str){
+		if(str != null && str.trim().length() >0){
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 判断集合的有效性
+	 * @param col
+	 * @return
+	 */
+	public static boolean isValid(Collection col){
+		if(col != null && !col.isEmpty()){
+			return true;
+		}
+		return false;
+	}
+}
